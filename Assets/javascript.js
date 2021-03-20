04d7b6f7723046de5c77c09f06ab39bb24005363
Sergio Torres <sergio.lo.torres@gmail.com>
2021-03-20 15:51:14 -0500

    innitial setup

---
+++ Assets/javascript.js
@@ -0,0 +1,43 @@
+var Apikey = "236654656475";
+
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+//make an account to open weather app
+
+
+//get weather data from any city!
+
+function getweatherdata(city){
+
+    //var city = 'austin'
+    //function get weatherdata (city)
+    //get weatherdata (austin)
+
+    //we could use Template Strings!!!
+    // 
+
+
+
+//we will need to write a fetch request
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=austin&units=imperial&appid"+ Apikey)
+    .then(function(response){
+        console.log(response);
+        return response.json();
+    }).then(function(data){
+        // 
+        //var farenheith = (data.main.temp - 274/15)*9/5 + 32;
+        $("#weather-data").empty();
+        $("#weather-data").append("<li>" + data.main.temp + "</li>");
+        //console.log d(ata)
+    });
+
+}
+//this will be our default value when app loads
+getweatherdata('austin');
+
+
+
+$("#submit").on("click", function(){
+    var city = $("#city").val().trim();
+    getweatherdata(city);
+})
