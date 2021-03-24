0b7aaba49c6ece798dfe9ba9b81aa92c1fc4149c
Sergio Torres <sergio.lo.torres@gmail.com>
2021-03-24 00:31:28 -0500

    weather cards updated - working on formating cards

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -26,7 +26,7 @@ function getweatherdata(city) {
             $("#current-weather").append("<img id='weatherIcon' src=" + imageLink + ">");
 
             //get weather data.
-            $("#current-weather").append("<li>" + data.weather[0].description + "</li><br>");
+            $("#current-weather").append("<h6>" + data.weather[0].description + "</h6><br>");
             $("#current-weather").append("<li>" + "Temperature: " + data.main.temp + " °F" + "</li>");
             $("#current-weather").append("<li>" + "Min-Max: " + data.main.temp_min + " °F" + " - " + data.main.temp_max + " °F" + "</li>");
             $("#current-weather").append("<li>" + "Humidity: " + data.main.humidity + " %" + "</li>");
@@ -63,27 +63,26 @@ function getweatherdata(city) {
                 var dailyWeatherCode = dailyWeather[i].weather[0].icon;
                 var dailyImageLink = "http://openweathermap.org/img/wn/" + dailyWeatherCode + "@2x.png";
                 var Icon =  $("<img>").attr('src', dailyImageLink ).addClass("card-img-top");
-                
                 //date and format it to MMDDYYY
-                var Dte = $("<h5>").addClass("card-title").text(new Date(dailyWeather[i].dt*1000).toLocaleDateString());
+                var Dte = $("<h5>").addClass("card-title text-center p-2").text(new Date(dailyWeather[i].dt*1000).toLocaleDateString());
                 //weather conditions
-                var Condition = $("<li>").addClass("list-group-item p-3").text( dailyWeather[i].weather[0].description);
+                var Condition = $("<li>").addClass("list-group-item p-2 h6").text( dailyWeather[i].weather[0].description);
                 //Max and Min temps
-                var Minmax = $("<li>").addClass("list-group-item p-3").text("Min-Max : "+ dailyWeather[i].temp.min +" °F" + " - " + dailyWeather[i].temp.max + " °F")
+                var Minmax = $("<li>").addClass("list-group-item p-2").text("Min-Max : "+ dailyWeather[i].temp.min +" °F" + " - " + dailyWeather[i].temp.max + " °F")
                 //humidity
-                var Humidity = $("<li>").addClass("list-group-item p-3").text("Humidity : " + dailyWeather[i].humidity + " %");
+                var Humidity = $("<li>").addClass("list-group-item p-2").text("Humidity : " + dailyWeather[i].humidity + " %");
                 
                 //create div containers
                 $("#daily-weather").append(Card);
-                
-                var Body = $("<div>").addClass("card-body p-2").append(Dte,Condition,Minmax,Humidity);
-                var Card = $("<div>").addClass("card").attr("style", "width: 9rem;").append(Icon,Body);
+                var Ul =$("<ul>").addClass("list-group list-group-flush").append(Condition,Minmax,Humidity);
+                var Body = $("<div>").addClass("card-body p-2").append(Ul);
+                var Card = $("<div>").addClass("card").attr("style", "width: 9rem;").append(Dte,Icon,Body);
             }
         })
         .catch(function (error) {
             // set up so there is a default value//set city to be Austin and triger getweather.
             // plus the console log along with a pop up error message.
-            //how to prevent an error? or return the user to the default value?
+            // how to prevent an error? or return the user to the default value?
             console.log("oh no! This an error message... ", error);
             return
         })
